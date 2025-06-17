b82e90e7e0c2aad0ff573d4a81a2be0f4f81fada
Devika-sajeev1010 <167263843+Devika-sajeev1010@users.noreply.github.com>
2025-06-17 18:06:21 +0000

    variables and datatypes

---
+++ 01_basics/01_datatype.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all js code in newer version
+//alert("Hello") // wont work because we are using node js and not browser (alert is used to generate a pop up message )
+
+/* datatypes (primitive)
+1. number = 2^53
+2. bigint = for larger numbers
+3. string = ""
+4. boolean = true/false
+5. null = a standalone value (its type is - object)
+6. undefined = whose value is not specified
+
+(non primitive)
+object
+*/
+ console.log(typeof(null)) // output is object -(a draw back of js even though null 
+                           // is not an object ..becuase in earlier versions of 
+                           // js the type tag of both null and object were given as 0)
---
+++ 01_basics/01_variable.js
@@ -0,0 +1,16 @@
+const accountId = 2135436
+let accountEmailid = "d@gmail.com"
+var accountPasswd = "123456"
+accountCity = "Jaipur"
+let accountState
+
+//accountId = 123434  cannot be done as its a constant
+/*
+prefer not to use var due to issue in block scope and functional scope
+*/
+
+accountEmailid= "ssa@gamil.com"
+accountPasswd="788798"
+accountCity="bengaluru"
+console.log(accountId)
+console.table([accountId,accountEmailid,accountPasswd,accountCity,accountState]);

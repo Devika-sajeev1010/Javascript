d9b8c64c9937227a9fe6f0a1156253d84c2ea12f
Devika-sajeev1010 <167263843+Devika-sajeev1010@users.noreply.github.com>
2025-06-28 16:47:31 +0000

    functions

---
+++ 03_basics/02_function.js
@@ -0,0 +1,15 @@
+function calculatecartprice(...num1) // with ... the values goes into an array
+{
+   return num1
+}
+console.log(calculatecartprice(100 , 200 , 300 , 20000))
+const user = {
+    username: "Dev",
+    price: 109
+}
+function handleObject(anyobject)
+{
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+
+handleObject(user)

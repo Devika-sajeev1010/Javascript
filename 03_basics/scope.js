0e475ff9726bb0135adb8d38c29baecda6e90248
Devika-sajeev1010 <167263843+Devika-sajeev1010@users.noreply.github.com>
2025-07-02 15:06:32 +0000

    scope

--- 03_basics/scope.js
+++ 03_basics/scope.js
@@ -2,9 +2,11 @@
  {
     let a =10
     const b =60
-    c = 70
+     var c = 70
  }
- console.log(a)
+ console.log(a) // it will show that a ,b are not defined.
  console.log(b)
- console.log(c)
+ console.log(c) // but it will print 70 due to var keyword
+//  Function Scope (var): Lives as long as the function runs.
+// Block Scope (let, const): Lives only inside { ... } like if, for, or {}.
  

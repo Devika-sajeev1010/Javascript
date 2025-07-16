451eff46f6c876928a4a093c3ca294d26d0e95d9
Devika-sajeev1010 <167263843+Devika-sajeev1010@users.noreply.github.com>
2025-07-16 17:20:50 +0000

    control flow

---
+++ controlflow/1_java.js
@@ -0,0 +1,38 @@
+//if
+if( 2 =="2") // it will print ececuted as it does not check the datatype
+{
+    console.log("executed"); 
+    
+}
+if( 2 === "2") // it will not  print ececuted as it does check the datatype (===)
+{
+    console.log("executed"); 
+    
+};
+// const usermail = "abc.gmail.com";
+// if(usermail)
+// {
+//     console.log("email found");
+// }
+// else{
+//     console.log("mail not found");
+// } ans mail found
+
+// const usermail = [];
+// if(usermail)
+// {
+//     console.log("email found");
+// }
+// else{
+//     console.log("mail not found");
+// } ans mail found
+
+// const usermail = "";
+// if(usermail)
+// {
+//     console.log("email found");
+// }
+// else{
+//     console.log("mail not found");
+// } ans mail  not found
+//false value = false , 0 , -0 , big int 0n , null .undefined, NaN

0ee5799a2ca7e728a17ae6963b6f84d2718091cc
Devika-sajeev1010 <167263843+Devika-sajeev1010@users.noreply.github.com>
2025-07-18 23:12:11 +0530

    Update 01_datatype.js

--- 01_basics/01_datatype.js
+++ 01_basics/01_datatype.js
@@ -1,7 +1,9 @@
 "use strict"; // treat all js code in newer version
-//alert("Hello") // wont work because we are using node js and not browser (alert is used to generate a pop up message )
+//alert("Hello") // wont work because we are using node js and not browser
+//(alert is used to generate a pop up message )
 
-/* datatypes (primitive)
+/* datatypes (primitive / immutable– you can’t change them directly.)
+//These are the most basic data types. They store single values and are copied by value.
 1. number = 2^53
 2. bigint = for larger numbers
 3. string = ""
@@ -9,9 +11,17 @@
 5. null = a standalone value (its type is - object)
 6. undefined = whose value is not specified
 
-(non primitive)
+(non primitive-mutable)
+//These store collections of values or complex entities. They are copied by reference.
+
 object
 */
  console.log(typeof(null)) // output is object -(a draw back of js even though null 
                            // is not an object ..becuase in earlier versions of 
                            // js the type tag of both null and object were given as 0)
+// Object	{name: "Devika", age: 21}	Key-value pairs
+// Array	[1, 2, 3]	Ordered list of values
+// Function	function() {}	Block of reusable code
+// Date	new Date()	Built-in object to handle dates/times
+// RegExp	/\d+/	Regular expressions
+// Map, Set	new Map(), new Set()	Advanced collections

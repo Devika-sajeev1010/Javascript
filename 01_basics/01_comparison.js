33c54ffa4ca4e39a584857b24f907833651d9c4a
Devika-sajeev1010 <167263843+Devika-sajeev1010@users.noreply.github.com>
2025-07-18 23:05:22 +0530

    Update 01_comparison.js

--- 01_basics/01_comparison.js
+++ 01_basics/01_comparison.js
@@ -1,7 +1,7 @@
 // in java script the comparison of two different datatypes is unpredictable
 console.log("2" > 1)
 console.log(2 > "5") // in this both string datatypes are converted into integers
-// it js coercion behaviour
+// it is js coercion behaviour
 console.log(null > 0) //false
 console.log(null == 0) // false
 console.log(null >= 0) //true
@@ -9,3 +9,13 @@ console.log(null >= 0) //true
 //  check ( <, > , <=, >=) works differently
 // comparison check converts null to 0 and in equality check null
 //  loosely represent undefined and not 0 
+//'10' > '5'	false (string comparison, lexicographic) - lexicograph In JavaScript,
+//when both operands are strings, comparison like > or < is done lexicographically, i.e.,
+//character by character, based on Unicode values, NOT numerically.
+//Objects and arrays are compared by reference, not value.
+// const a = { name: "JS" };
+// const b = { name: "JS" };
+
+// a == b     // false (different memory references)
+// a === b    // false
+// a === a    // true
